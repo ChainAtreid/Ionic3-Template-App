a1fdf8a68217efb1850bba7de319d0633371dd4b
unknown <katerina.hufova@email.cz>
2017-09-12 10:57:36 +0200

    fix

--- src/app/env.constants.ts
+++ src/app/env.constants.ts
@@ -1,12 +1,14 @@
 export const Config = {
-  firebase: {
+  {
     apiKey: "AIzaSyCcKGVwIrpQXCk_zHomTyWb5GMhwv2eRns",
     authDomain: "my-new-project-778a8.firebaseapp.com",
     databaseURL: "https://my-new-project-778a8.firebaseio.com",
     projectId: "my-new-project-778a8",
     storageBucket: "my-new-project-778a8.appspot.com",
     messagingSenderId: "800004565623"
-  }
+  },
+  google_web_client_id: "904616137099-730tr4g3m6mhfldojejlren97nk3ckod.apps.googleusercontent.com",
+  firebase_tables: {
     User: 'users',
     Tasks: 'tasks'
   }
